26a7260eb49e6936597e8992b28c7a45dcc0b6e7
Tiago Galvão M. Freire <tiagogmfreire@gmail.com>
2019-02-23 23:06:37 -0300

    Adicionando seed de banco de dados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,51 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\UFModel;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Classe para povoar as tabelas com dados iniciais.
+     * 
+     * Povoa os dados da tabela de domínio de UFs.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $ufs = [
+            ['uf' => 'AC', 'descricao' => 'Acre'],
+            ['uf' => 'AL', 'descricao' => 'Alagoas'],
+            ['uf' => 'AP', 'descricao' => 'Amapá'],
+            ['uf' => 'AM', 'descricao' => 'Amazonas'],
+            ['uf' => 'BA', 'descricao' => 'Bahia'],
+            ['uf' => 'CE', 'descricao' => 'Ceará'],
+            ['uf' => 'DF', 'descricao' => 'Distrito Federal'],
+            ['uf' => 'ES', 'descricao' => 'Espírito Santo'],
+            ['uf' => 'GO', 'descricao' => 'Goiás'],
+            ['uf' => 'MA', 'descricao' => 'Maranhão'],
+            ['uf' => 'MT', 'descricao' => 'Mato Grosso'],
+            ['uf' => 'MS', 'descricao' => 'Mato Grosso do Sul'],
+            ['uf' => 'MG', 'descricao' => 'Minas Gerais'],
+            ['uf' => 'PA', 'descricao' => 'Pará'],
+            ['uf' => 'PB', 'descricao' => 'Paraíba'],
+            ['uf' => 'PR', 'descricao' => 'Paraná'],
+            ['uf' => 'PE', 'descricao' => 'Pernambuco'],
+            ['uf' => 'PI', 'descricao' => 'Piauí'],
+            ['uf' => 'RJ', 'descricao' => 'Rio de Janeiro'],
+            ['uf' => 'RN', 'descricao' => 'Rio Grande do Norte'],
+            ['uf' => 'RS', 'descricao' => 'Rio Grande do Sul'],
+            ['uf' => 'RO', 'descricao' => 'Rondônia'],
+            ['uf' => 'RR', 'descricao' => 'Roraima'],
+            ['uf' => 'SC', 'descricao' => 'Santa Catarina'],
+            ['uf' => 'SP', 'descricao' => 'São Paulo'],
+            ['uf' => 'SE', 'descricao' => 'Sergipe'],
+            ['uf' => 'TO', 'descricao' => 'Tocantins']            
+        ];
+
+        foreach ($ufs as $uf) {
+           UFModel::create($uf);
+        }
     }
 }
