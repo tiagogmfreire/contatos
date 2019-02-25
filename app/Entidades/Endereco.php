a68e6a23d41ed0e38ecdf73274d75cc388097fb9
tiago.freire <tiago.freire@castgroup.com.br>
2019-02-25 16:34:50 -0300

    Iniciando simplificação da arquitetura

--- app/Entidades/Endereco.php
+++ app/Entidades/Endereco.php
@@ -13,4 +13,67 @@ class Endereco extends \stdClass
     public $bairro;
     public $cidade;
     public $cep;
+
+    protected $model;
+
+    public function __construct(EnderecoModel $enderecoModel)
+    {
+        $this->model = $enderecoModel;
+    }
+
+    public function listar()
+    { 
+        return EnderecoModel::all();
+    }
+
+    public function detalhar($id)
+    {
+        return EnderecoModel::find($id);
+    }
+
+    public function criar()
+    { 
+        $this->model->pessoa_id = $this->pessoa_id;
+        $this->model->uf_id = $this->buscarUF($this->uf);
+        $this->model->logradouro = $this->logradouro;
+        $this->model->complemento = $this->complemento;
+        $this->model->bairro = $this->bairro;
+        $this->model->cidade = $this->cidade;
+        $this->model->cep = $this->cep;
+
+        return $this->model->save();
+    }
+
+    public function atualizar()
+    {
+        $this->model = EnderecoModel::find($this->id);
+
+        $this->model->pessoa_id = $this->pessoa_id;
+        $this->model->uf_id = $this->buscarUF($this->uf);
+        $this->model->logradouro = $this->logradouro;
+        $this->model->complemento = $this->complemento;
+        $this->model->bairro = $this->bairro;
+        $this->model->cidade = $this->cidade;
+        $this->model->cep = $this->cep;
+
+        return $this->model->save();
+    }
+    
+    public function excluir($id)
+    {
+        $this->model = EnderecoModel::find($id);
+
+        //realizando exclusão lógica do registro
+        $this->model->delete();
+
+        //persistindo alterações
+        return $this->model->save();
+    }
+
+    protected function buscarUF($uf)
+    {
+        $uf = UFModel::where('uf',$uf)->first();
+
+        return $uf->id;
+    }
 }
--- app/Entidades/Pessoa.php
+++ app/Entidades/Pessoa.php
@@ -9,4 +9,54 @@ class Pessoa extends \stdClass
     public $cpf;
     public $email;
     public $telefone;
+
+    protected $model;
+
+    public function __construct(PessoaModel $pessoaModel)
+    {
+        $this->model = $pessoaModel;
+    }
+
+    public function listar()
+    {         
+        return PessoaModel::all();
+    }
+
+    public function detalhar($id)
+    {
+        return PessoaModel::find($id)->with('enderecos')->first();
+    }
+
+    public function criar()
+    { 
+        $this->model->nome = $this->nome;
+        $this->model->cpf = $this->cpf;
+        $this->model->email = $this->email;
+        $this->model->telefone = $this->telefone;
+
+        return $this->model->save();
+    }
+
+    public function atualizar()
+    {
+        $this->model = PessoaModel::find($this->id);
+
+        $this->model->nome = $this->nome;
+        $this->model->cpf = $this->cpf;
+        $this->model->email = $this->email;
+        $this->model->telefone = $this->telefone;
+
+        return $this->model->save();
+    }
+    
+    public function excluir($id)
+    {
+        $this->model = PessoaModel::find($id);
+
+        //realizando exclusão lógica do registro
+        $this->model->delete();
+
+        //persistindo alterações
+        return $this->model->save();
+    }
 }
